4ee33bf68ffca67c3da482504ea7595e95d1bd80
Digital Space X <digitalspacex.ofc@gmail.com>
2025-10-29 01:22:19 +0000

    adiciona o pixel do facebook "do jeito que está" no <head> do site.
    scr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Montserrat, Open_Sans } from 'next/font/google';
+import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from '@/lib/utils';
@@ -33,9 +34,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={cn(openSans.variable, montserrat.variable)}>
-      <body className="font-body antialiased">
-        {children}
-        <Toaster />
+      <body>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1338555780453895');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img height="1" width="1" style={{display: 'none'}}
+            src="https://www.facebook.com/tr?id=1338555780453895&ev=PageView&noscript=1"
+          />
+        </noscript>
+        <main className="font-body antialiased">
+          {children}
+          <Toaster />
+        </main>
       </body>
     </html>
   );
