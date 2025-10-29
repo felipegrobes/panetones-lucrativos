c3f35b84443018589513655b14bfedf20739fad6
Digital Space X <digitalspacex.ofc@gmail.com>
2025-10-29 01:03:12 +0000

    tira isso (_for element <Header>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,13 +9,11 @@ import { PainPoints } from '@/components/landing/PainPoints';
 import { Faq } from '@/components/landing/Faq';
 import { Footer } from '@/components/landing/Footer';
 import { SectionSeparator } from '@/components/landing/SectionSeparator';
-import { Header } from '@/components/landing/Header';
 import { InstagramPrompterSection } from '@/components/landing/InstagramPrompterSection';
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col overflow-x-hidden">
-      <Header />
       <main className="flex-1">
         <Hero />
         <SectionSeparator />
